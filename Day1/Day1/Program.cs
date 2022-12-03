daeb74b4d9ecf8d8fe74e144cffc394c0c2fcf37
Arnout Reitsma <arnout.reitsma@outlook.com>
2022-12-03 13:57:23 +0100

    day2

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -2,29 +2,8 @@
 using System.Diagnostics.Metrics;
 using System.Reflection;
 
-List<int> top3MostCalories = new() { 0, 0, 0 };
-int calories = 0;
+char[,] games = File.ReadAllText(@"..\..\..\input.txt").Split("\r\n").Select(s => s.Split()).Select(l => new { l[0], l[2] });
 
-foreach (string line in System.IO.File.ReadLines(@"C:\Users\Arnout Reitsma\source\repos\AdventOfCode2022\Day1\Day1\input.txt"))
-{
-    if (line == string.Empty)
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            if (calories > top3MostCalories[i])
-            {
-                top3MostCalories.Insert(i, calories);
-                top3MostCalories.RemoveAt(3);
-                break;
-            }
-        }
 
-        calories = 0;
-        continue;
-    }
-    calories += int.Parse(line);
-}
-
-System.Console.WriteLine("Top 3 MostCalories {0}", top3MostCalories.Sum());
 // Suspend the screen.  
 System.Console.ReadLine();
---
+++ Day1/Day2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+using System.Reflection;
+
+List<int> top3MostCalories = new() { 0, 0, 0 };
+int calories = 0;
+
+foreach (string line in System.IO.File.ReadLines(@"..\..\..\input.txt"))
+{
+
+}
+
+System.Console.WriteLine("Top 3 MostCalories {0}", top3MostCalories.Sum());
+// Suspend the screen.  
+System.Console.ReadLine();
