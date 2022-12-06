d3360b163e58543e2f991f4590888ae2983fa5fd
Arnout Reitsma <arnout.reitsma@infi.nl>
2022-12-06 16:33:41 +0100

    day2 right

--- Day1/Day2/Program.cs
+++ Day1/Day2/Program.cs
@@ -2,14 +2,39 @@
 using System.Diagnostics.Metrics;
 using System.Reflection;
 
-List<int> top3MostCalories = new() { 0, 0, 0 };
-int calories = 0;
+// A / X = ROCK
+// B / Y = PAPER
+// C / Z = SCISSORS
 
+int win = 6;
+int loss = 0;
+int draw = 3;
+
+int rock = 1;
+int paper = 2;
+int scissors = 3;
+
+var conditions = new List<Condition>()
+{
+    new Condition(new string[] { "A", "X" }, draw + rock, loss + scissors),
+    new Condition(new string[] { "B", "Y" }, draw + paper, draw + paper),
+    new Condition(new string[] { "C", "Z" }, draw + scissors, win + rock),
+    new Condition(new string[] { "A", "Y" }, win + paper, draw + rock),
+    new Condition(new string[] { "B", "Z" }, win + scissors, win + scissors),
+    new Condition(new string[] { "C", "X" }, win + rock, loss + paper),
+    new Condition(new string[] { "A", "Z" }, loss + scissors, win + paper),
+    new Condition(new string[] { "B", "X" }, loss + rock, loss + rock),
+    new Condition(new string[] { "C", "Y" }, loss + paper, draw + scissors),
+};
+int scorePartOne = 0;
+int scorePartTwo = 0;
 foreach (string line in System.IO.File.ReadLines(@"..\..\..\input.txt"))
 {
-
+    var split = line.Split();
+    var condition = conditions.Single(x => x.Game.SequenceEqual(split));
+    scorePartOne += condition.Points;
+    scorePartTwo += condition.PointsPartTwo;
 }
-
-System.Console.WriteLine("Top 3 MostCalories {0}", top3MostCalories.Sum());
-// Suspend the screen.  
-System.Console.ReadLine();
+System.Console.WriteLine(scorePartOne);
+System.Console.WriteLine(scorePartTwo);
+record Condition(string[] Game, int Points, int PointsPartTwo);
