3fbae53cfea33d2f281bdd914a30d66902245cc5
Arnout Reitsma <arnout.reitsma@outlook.com>
2022-12-11 21:39:35 +0100

    day6

--- Day1/Day5/Program.cs
+++ Day1/Day5/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Reflection;
-using System.Security;
 
 Console.WriteLine("Hello, Day5!");
 
---
+++ Day1/Day6/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Day6!");
+
+var inputText = System.IO.File.ReadAllText(@"..\..\..\input.txt");
+
+//var startPacketLength = 4;
+var startPacketLength = 14;
+
+for(int i = 0; i < inputText.Length; i++)
+{
+    if(inputText.Substring(i, startPacketLength).Distinct().Count() == startPacketLength)
+    {
+        Console.WriteLine($"First start-of-packet at: {i+ startPacketLength}");
+        return;
+    }
+}
